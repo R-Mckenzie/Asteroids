2eaf63ad7a94f039230f34722d547f8ab1b7385e
Ross Mckenzie <rossmckenzie11@gmail.com>
2024-02-23 21:48:59 +0000

    Adds highscores and documentation

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/R-Mckenzie/go-engine/engine"
 	"github.com/go-gl/mathgl/mgl32"
 )
+
+const highscores_file = "highscores.txt"
 
 func main() {
 	game := engine.CreateGame(600, 400)
@@ -34,36 +40,79 @@ func main() {
 		asteroidBigTexture:    asteroidBig,
 		asteroidSmallTexture:  asteroidSmall,
 		asteroidTimer:         time.Now(),
-		asteroidSpawnInterval: time.Second * 2,
+		asteroidSpawnInterval: time.Millisecond * 1500,
 	}
 
-	game.SetScene(inGame)
-	menu := &menuScene{game: game, gameScene: inGame}
+	highscores := []highscoreEntry{}
+
+	// read highscores file if it exists
+	scoreData, err := os.ReadFile(highscores_file)
+	if err != nil {
+		fmt.Println("Could not load highscores")
+	} else {
+		lines := strings.Split(string(scoreData), "\n")
+		for _, v := range lines {
+			if len(strings.TrimSpace(v)) == 0 {
+				break
+			}
+			row := strings.Split(v, ",")
+			name := row[0]
+			score, err := strconv.Atoi(row[1])
+			if err != nil {
+				fmt.Println("could not read score")
+				continue
+			}
+			highscores = append(highscores, highscoreEntry{name, score})
+		}
+	}
+
+	// Sort highscores into descending order
+	slices.SortFunc(highscores, func(a, b highscoreEntry) int { return b.score - a.score })
+	menu := &menuScene{game: game, gameScene: inGame, highscores: highscores}
+	inGame.menu = menu
+
 	game.SetScene(menu)
 	game.Run()
 }
 
+type highscoreEntry struct {
+	name  string
+	score int
+}
+
 type menuScene struct {
-	game      *engine.Game
-	gameScene *gameScene
+	highscores []highscoreEntry
+	game       *engine.Game
+	gameScene  *gameScene
 }
 
 func (m *menuScene) Update() {
+	engine.Renderer.BeginScene(engine.NewCamera2D(0, 0), mgl32.Vec3{1, 1, 1}, 1)
 	engine.UI.Begin()
 	engine.UI.Label("ASTEROIDS", 60, 30, 120, mgl32.Vec4{1, 1, 1, 1})
-	if engine.UI.Button(200, 200, 200, 50, "PLAY", mgl32.Vec4{1, 1, 1, 1}) {
+	if engine.UI.Button(200, 130, 200, 30, "PLAY", mgl32.Vec4{1, 1, 1, 1}) {
 		m.game.SetScene(m.gameScene)
 		m.gameScene.isPlaying = true
 	}
 
-	if engine.UI.Button(200, 300, 200, 50, "QUIT", mgl32.Vec4{1, 1, 1, 1}) {
+	if engine.UI.Button(200, 170, 200, 30, "QUIT", mgl32.Vec4{1, 1, 1, 1}) {
 		m.game.Quit()
 	}
+
+	engine.UI.Label("HIGHSCORES: ", 200, 210, 32, mgl32.Vec4{1, 1, 1, 1})
+	for i, r := range m.highscores {
+		if i >= 5 {
+			break
+		}
+		engine.UI.Label(fmt.Sprintf("%d. %s: %d", i+1, r.name, r.score), 200, float32(240+(i*30)), 32, mgl32.Vec4{1, 1, 1, 1})
+	}
+
 	engine.UI.End()
 }
 
 type gameScene struct {
 	game *engine.Game
+	menu *menuScene
 
 	bulletTexture        engine.Texture
 	asteroidBigTexture   engine.Texture
@@ -86,6 +135,7 @@ type entity struct {
 	direction mgl32.Vec2
 }
 
+// Spaws an asteroid at a random point outside the screen, travelling into the middle
 func (s *gameScene) spawnAsteroid() {
 	size := rand.Intn(2)
 	side := rand.Intn(4) // 0,1,2,3 starting left going clockwise for which side so spawn in
@@ -121,10 +171,12 @@ func (s *gameScene) spawnAsteroid() {
 var playerSpeed float32 = 0.1
 var asteroidSpeed float32 = 2
 var rotSpeed float32 = 0.2
+var username string
 
 func (s *gameScene) Update() {
 	if s.isPlaying {
-		// MOVEMENT
+
+		// PLAYER LOGIC
 		if engine.Input.KeyDown(engine.KeyW) {
 			s.playerVel = s.playerVel.Add(s.playerDir.Mul(playerSpeed))
 			engine.LoopSound("engine", -1)
@@ -154,6 +206,7 @@ func (s *gameScene) Update() {
 
 		s.player.Pos = s.player.Pos.Add(mgl32.Vec3(s.playerVel.Vec3(0)))
 
+		// If ship goes off the end of the screen, wrap it to the other side
 		if s.player.Pos.X() < 0 {
 			s.player.Pos[0] = 600
 		} else if s.player.Pos.X() > 600 {
@@ -166,7 +219,7 @@ func (s *gameScene) Update() {
 			s.player.Pos[1] = 0
 		}
 
-		// SHOOTING
+		// SHOOTING / BULLET LOGIC
 		if engine.Input.KeyOnce(engine.KeySpace) {
 			sprite := engine.NewSprite(32, 32, s.player.Pos[0], s.player.Pos[1], 1, s.bulletTexture, nil)
 			s.bullets = append(s.bullets, &entity{sprite: &sprite, direction: s.playerDir})
@@ -186,6 +239,7 @@ func (s *gameScene) Update() {
 			b.sprite.Pos = b.sprite.Pos.Add(b.direction.Vec3(0).Mul(8))
 		}
 
+		// ASTEROID LOGIC
 		if time.Now().Sub(s.asteroidTimer) > s.asteroidSpawnInterval {
 			s.spawnAsteroid()
 			s.asteroidTimer = time.Now()
@@ -195,7 +249,6 @@ func (s *gameScene) Update() {
 			}
 		}
 
-		// ASTEROIDS
 		for ia, a := range s.asteroids {
 			if collides(*a.sprite, *s.player) {
 				s.isPlaying = false
@@ -212,6 +265,7 @@ func (s *gameScene) Update() {
 				continue
 			}
 
+			// IF ASTEROID SHOT
 			for ib, b := range s.bullets {
 				if collides(*a.sprite, *b.sprite) {
 					engine.PlaySound("explosion", -1)
@@ -238,24 +292,51 @@ func (s *gameScene) Update() {
 
 	engine.UI.Begin()
 	engine.UI.Label(fmt.Sprintf("Score: %d", s.score), 10, 10, 32, mgl32.Vec4{1, 1, 1, 1})
+
 	if !s.isPlaying {
-		engine.UI.Label(fmt.Sprintf("GAME OVER"), 150, 100, 64, mgl32.Vec4{1, 1, 1, 1})
-		if (engine.UI.Button(200, 150, 200, 100, "RESTART", mgl32.Vec4{1, 1, 1, 1})) {
-			s.player.Pos = mgl32.Vec3{300, 200, 3}
-			s.player.Rot = mgl32.Vec3{}
-			s.playerDir = mgl32.Vec2{1, 0}
-			s.playerVel = mgl32.Vec2{0, 0}
-			s.asteroidSpawnInterval = time.Second * 2
-			s.bullets = []*entity{}
-			s.asteroids = []*entity{}
-			s.score = 0
-			s.isPlaying = true
+		engine.UI.Label(fmt.Sprintf("GAME OVER"), 160, 100, 64, mgl32.Vec4{1, 1, 1, 1})
+
+		engine.UI.TextInput("NAME", 200, 160, 15, 32, &username)
+		if (engine.UI.Button(200, 200, 200, 30, "SAVE SCORE", mgl32.Vec4{1, 1, 1, 1})) {
+			f, err := os.OpenFile(highscores_file, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+
+			if _, err = f.WriteString(fmt.Sprintf("%s,%d\n", username, s.score)); err != nil {
+				panic(err)
+			}
+			s.restartGame()
+			s.game.SetScene(s.menu)
 		}
-		if (engine.UI.Button(200, 250, 200, 100, "QUIT", mgl32.Vec4{1, 1, 1, 1})) {
+
+		if (engine.UI.Button(200, 240, 200, 30, "RESTART", mgl32.Vec4{1, 1, 1, 1})) {
+			s.restartGame()
+		}
+
+		if (engine.UI.Button(200, 280, 200, 30, "MAIN MENU", mgl32.Vec4{1, 1, 1, 1})) {
+			s.restartGame()
+			s.game.SetScene(s.menu)
+		}
+
+		if (engine.UI.Button(200, 320, 200, 30, "QUIT", mgl32.Vec4{1, 1, 1, 1})) {
 			s.game.Quit()
 		}
 	}
 	engine.UI.End()
+}
+
+func (s *gameScene) restartGame() {
+	s.player.Pos = mgl32.Vec3{300, 200, 3}
+	s.player.Rot = mgl32.Vec3{}
+	s.playerDir = mgl32.Vec2{1, 0}
+	s.playerVel = mgl32.Vec2{0, 0}
+	s.asteroidSpawnInterval = time.Second * 2
+	s.bullets = []*entity{}
+	s.asteroids = []*entity{}
+	s.score = 0
+	s.isPlaying = true
 }
 
 func collides(a, b engine.Sprite) bool {
